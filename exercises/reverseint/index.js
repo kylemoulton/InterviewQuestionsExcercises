54c4a1b3890ff106f78b47fd95d657a106837eba
Kyle Moulton <kylealexmoulton@gmail.com>
2018-02-07 12:47:16 -0800

    reverseint complete

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//     if (n < 0) {
+//         return -1 * Number(n.toString().substring(1).split('').reverse().join(''));
+//     } else {
+//         return Number(n.toString().split('').reverse().join(''));
+//     }
+// }
+
+function reverseInt(n) {
+    return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''));
+}
 
 module.exports = reverseInt;
