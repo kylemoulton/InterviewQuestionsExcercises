5326f7d027e9e21f6c551d4729f467ed73554eb0
Kyle Moulton <kylealexmoulton@gmail.com>
2018-02-12 19:05:17 -0800

    steps complete

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -33,6 +33,7 @@ function steps(n) {
 }
 */
 
+/*
 function steps(n) {
     for (let i = 0; i < n; i++) {
         let row = '';
@@ -45,6 +46,65 @@ function steps(n) {
         }     
         console.log(row);
     }
+}
+*/
+
+/*
+function steps(n) {
+    printRow(n, n);    
+}
+
+function printRow(currentRow, cols = 0) {
+    if (currentRow === 0) {
+        return;
+    } else {
+        let toPrint = '';
+        for (let i = 0; i < cols; i++) {
+            if (cols - currentRow >= i) {
+                toPrint += '#';
+            } else {
+                toPrint += ' ';
+            }
+        }
+        console.log(toPrint);
+        printRow(currentRow - 1, cols);
+    }
+}
+*/
+
+/*
+function steps(n, currentRow = 0) {
+    if (n === currentRow) {
+        return;
+    } else {
+        let toPrint = '';
+        for (let i = 0; i < n; i++) {
+            if (currentRow - i >= 0) {
+                toPrint += '#';
+            } else {
+                toPrint += ' ';
+            }
+        }
+        console.log(toPrint);
+        steps(n, currentRow + 1);
+    }
+}
+*/
+
+function steps(n, currentRow = 0, toPrint = '') {
+    if (n === currentRow) {
+        return;
+    } 
+    if (n === toPrint.length) {
+        console.log(toPrint);
+        return steps(n, currentRow + 1);
+    }
+    if (toPrint.length <= currentRow) {
+        toPrint += '#';
+    } else {
+        toPrint += ' ';
+    }   
+    steps(n, currentRow, toPrint);
 }
 
 module.exports = steps;
