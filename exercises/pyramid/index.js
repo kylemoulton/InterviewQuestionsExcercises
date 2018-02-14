fe830f83f6b4f663fed288a3b6ec7ea01f55cf29
Kyle Moulton <kylealexmoulton@gmail.com>
2018-02-14 10:00:15 -0800

    pyramid completed

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,7 +14,6 @@
 //       ' ### '
 //       '#####'
 
-
 /*
             X's     Spaces          
     X    	0 - 1 	0 - 8 4/4       
@@ -28,6 +27,7 @@ X's to print => 2 * i + 1
 Spaces on right while length > (n - i - 1) + (2i + 1) - 1 = n + i - 1
 */
 
+/*
 function pyramid(n) {
     for (let i = 0; i < n; i++) {
         let toPrint = '';
@@ -40,6 +40,78 @@ function pyramid(n) {
         }
         console.log(toPrint);
     }
+}
+*/
+
+/*
+function pyramid(n) {
+    for (let i = 0; i < n; i++) {
+        let toPrint = '';
+        for (let j = 0; j < 2 * n - 1; j++) {
+            if (toPrint.length >= n - i - 1 && toPrint.length < n + i) {
+                toPrint += '#';
+            } else {
+                toPrint += ' ';
+            }
+        }
+        console.log(toPrint);
+    }
+}
+*/
+
+/*
+function pyramid(n) {
+    const midPoint = Math.floor((2 * n - 1) / 2);
+    for (let i = 0; i < n; i++) {
+        let toPrint = '';
+        for (let j = 0; j < 2 * n - 1; j++) {
+            if (midPoint - i <= j && midPoint + i >= j) {
+                toPrint += '#';
+            } else {
+                toPrint += ' ';
+            }
+        }
+        console.log(toPrint);
+    }
+}
+*/
+
+/*
+function pyramid(n, currentRow = 0, toPrint = '') {
+    if (n === currentRow) {
+        return;
+    }
+    if (2 * n - 1=== toPrint.length) {
+        console.log(toPrint);
+        return pyramid(n, currentRow + 1);
+    }
+    if (toPrint.length >= Math.floor((2 * n - 1) / 2) - currentRow &&
+        toPrint.length <= Math.floor((2 * n - 1) / 2) + currentRow) {
+        toPrint += '#';
+    } else {
+        toPrint += ' ';
+    } 
+    pyramid(n, currentRow, toPrint);
+}
+*/
+
+function pyramid(n, currentRow = 0, toPrint = '') {
+    if (n === currentRow) {
+        return;
+    }
+    if (toPrint.length === 2 * n - 1) {
+        console.log(toPrint);
+        return pyramid(n, currentRow + 1)
+    }
+    const midPoint = Math.floor((2 * n - 1) / 2);
+    let toAdd;
+    if (midPoint - currentRow <= toPrint.length &&
+        midPoint + currentRow >= toPrint.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    pyramid(n, currentRow, toPrint + add);
 }
 
 module.exports = pyramid;
