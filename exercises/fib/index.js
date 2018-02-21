9f4e5ea13d412bdd8b0c4305f619e70e9ce10e86
Kyle Moulton <kylealexmoulton@gmail.com>
2018-02-21 07:58:06 -0800

    fib complete

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,7 @@
 // Example:
 //   fib(4) === 3
 
+// O(n)
 /*
 function fib(n) {
     let fibSeq = [0, 1];
@@ -18,7 +19,8 @@ function fib(n) {
 }
 */
 
-
+// O(n)
+/*
 function fib(n, arr = [0, 1], currentIndex = 2) {
     if (currentIndex > n) {
         return arr[n];
@@ -26,5 +28,40 @@ function fib(n, arr = [0, 1], currentIndex = 2) {
     arr[currentIndex] = arr[currentIndex - 2] + arr[currentIndex - 1];
     return fib(n, arr, currentIndex + 1);
 }
+*/
+
+// O(2^n)
+/*
+function fib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+*/
+
+// O(n)
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
