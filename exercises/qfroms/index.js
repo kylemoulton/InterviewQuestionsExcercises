1a0a67ef1277b0b00c4d9fd4dc8f8aad0a7d744c
Kyle Moulton <kylealexmoulton@gmail.com>
2018-02-22 13:15:05 -0800

    qfroms complete

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,29 +14,38 @@
 
 /*
 Conceptual notes:
-
+add 1 2 3 4 5
 [ ]     [1 2 3 4 5]
 
 [5 4 3 2]   []
 remove 1
+[]      [2 3 4 5]
 
 [5 4 3]     []
 remove 2
+[]      [3 4 5]
 
-[5 4 3]   [6 7 8 9]
+[]   [3 4 5 6 7 8 9]
 add 6 7 8 9
 
 
-[]  [6 7 8 9]
+[9 8 7 6 5]  []
 remove 3 4 5
+[]      [5 6 7 8 9]
 
 
 [9 8 7]     []
 remove 6
+[]      [7 8 9]
 */
 
 const Stack = require('./stack');
 
+// Major error, if you remove element from queue (or peek for that matter)
+// and then add more to the queue, removing aftwards will only give you the
+// first element added after the first remove or peek. Need to re-add all 
+// elements back to s1 after a remove or peek.
+/*
 class Queue {
     constructor() {
         this.s1 = new Stack();
@@ -63,6 +72,46 @@ class Queue {
             }
         }
         return this.s1.peek();
+    }
+}
+*/
+
+class Queue {
+    constructor() {
+        this.s1 = new Stack();
+        this.s2 = new Stack();
+    }
+
+    add(val) {
+        this.s2.push(val);
+    }
+
+    remove() {
+        while (this.s2.peek()) {
+            this.s1.push(this.s2.pop());
+        }
+
+        const toReturn = this.s1.pop();
+        
+        while (this.s1.peek()) {
+            this.s2.push(this.s1.pop());
+        }
+
+        return toReturn;
+    }
+
+    peek() {
+        while (this.s2.peek()) {
+            this.s1.push(this.s2.pop());
+        }
+
+        const toReturn = this.s1.peek();
+
+        while (this.s1.peek()) {
+            this.s2.push(this.s1.pop());
+        }
+
+        return toReturn;
     }
 }
 
