ba372b2113332901b3c3dd92afd8e9b9ff92908b
Kyle Moulton <kylealexmoulton@gmail.com>
2018-04-04 13:18:08 -0700

    validate complete

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,9 +5,9 @@
 // every node's right hand child is greater than
 // the parent
 
-
+/*
 function validate(node, min = null, max = null) {
-    if (((node.data > min) || !min) && ((node.data < max) || !max)) {
+    if ((!min || (node.data > min)) && (!max || (node.data < max))) {
         if (!node.left && !node.right) {
             return true;
         } else if (!node.right) {
@@ -20,6 +20,32 @@ function validate(node, min = null, max = null) {
     } else {
         return false;
     }
+}
+*/
+
+/*
+After comparing my solution to the provided solution below, I can't think
+of any major reasons why mine should be much less efficient, but mine
+is a bit more complex than it needs to be. In the future maybe I should 
+try to look for all of the cases where the validate or recurse should 
+terminate without another call to validate and implement those first.
+*/
+
+function validate(node, min = null, max = null) {
+    if (max !== null && node.data > max) {
+        return false;
+    }
+    if (min !== null && node.data < min) {
+        return false;
+    }
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+
+    return true;
 }
 
 module.exports = validate;
