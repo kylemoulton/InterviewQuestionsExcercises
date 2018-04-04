41f2b56d1a8b0891eda1a813887f309085ebed51
Kyle Moulton <kylealexmoulton@gmail.com>
2018-04-04 10:12:21 -0700

    validate attempted

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,21 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+
+function validate(node, min = null, max = null) {
+    if (((node.data > min) || !min) && ((node.data < max) || !max)) {
+        if (!node.left && !node.right) {
+            return true;
+        } else if (!node.right) {
+            return validate(node.left, min, node.data);
+        } else if (!node.left) {
+            return validate(node.right, node.data, max);
+        } else {
+            return (validate(node.left, min, node.data) && validate(node.right, node.data, max));
+        }
+    } else {
+        return false;
+    }
+}
 
 module.exports = validate;
