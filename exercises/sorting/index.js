3744d914bd0a84effc0663dfbfd2ae958146480e
Kyle Moulton <kylealexmoulton@gmail.com>
2018-04-05 14:57:54 -0700

    sorting - mergeSort attempted

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -22,19 +22,58 @@ function selectionSort(arr) {
                 smallestIndex = j;
             }
         }
-        let temp = arr[i];
-        arr[i] = arr[smallestIndex];
-        arr[smallestIndex] = temp;
+        if (smallestIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[smallestIndex];
+            arr[smallestIndex] = temp;
+        }
     }
     return arr;
 }
 
 function mergeSort(arr) {
+    if (arr.length > 1) {
+        let left = mergeSort(arr.slice(0, Math.ceil(arr.length / 2)));
+        let right = mergeSort(arr.slice(Math.ceil(arr.length / 2)));
+        return merge(left, right);
+    } else {
+        return arr;
+    }
+
 
 }
 
 function merge(left, right) {
+    let arr = [];
+    let currentIndex = 0;
 
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            arr[currentIndex] = left[i];
+            i++
+        } else {
+            arr[currentIndex] = right[j];
+            j++;
+        }
+        currentIndex++;
+    }   
+
+    while (i < left.length) {
+        arr[currentIndex] = left[i];
+        i++;
+        currentIndex++;
+    }
+
+    while (j < right.length) {
+        arr[currentIndex] = right[j]
+        j++;
+        currentIndex++;
+    }
+
+    return arr;
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
