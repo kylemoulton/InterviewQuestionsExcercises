d03ce3a79fe0e45ef63b29db1d2be8e1f6521c46
Kyle Moulton <kylealexmoulton@gmail.com>
2018-04-05 10:08:13 -0700

    sorting - bubble and selection attempted

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,11 +2,31 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j + 1 < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let smallestIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[smallestIndex] > arr[j]) {
+                smallestIndex = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[smallestIndex];
+        arr[smallestIndex] = temp;
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
